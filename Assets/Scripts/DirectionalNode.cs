96802592b362a0272e31dac5e0b2bfb752225e12
crazybingo <sam.politeburke@gmail.com>
2020-09-20 14:23:08 -0400

    Added directional node logic

---
+++ Assets/Scripts/DirectionalNode.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalNode : MonoBehaviour
+{
+    SpriteRenderer sr;
+    public Sprite none;
+    public Sprite left;
+    public Sprite right;
+    public string current;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = left;
+        SwitchSprite();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SwitchSprite()
+    {
+        if(current=="left")
+        {
+            Debug.Log("Choose Left");
+            sr.sprite = left;
+        }
+        else if(current=="right")
+        {
+            Debug.Log("Choose Right");
+            sr.sprite = right;
+
+        }
+        else
+        {
+            Debug.Log("Choose None");
+            sr.sprite = none;
+        }
+    }
+}
